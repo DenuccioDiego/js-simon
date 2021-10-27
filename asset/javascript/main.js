42399139ec5fc7f1f4f99a4c052f0eaf3019bf03
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-27 13:47:47 +0200

    numero random con alerts completo

---
+++ asset/javascript/main.js
@@ -0,0 +1,13 @@
+
+
+let numeroRandom = []
+
+for(i=0; i<=5; i++ ){
+
+     numero = Math.floor(Math.random()*100)
+     alert(numero)
+     numeroRandom.push(numero)
+}
+
+console.log(numeroRandom)
+

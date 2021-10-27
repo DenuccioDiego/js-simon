69a4c42ed0cd2937fe429ec597ff42e842cb2e6f
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-27 14:14:46 +0200

    secondo prompt con while inserito (Numeri che l'utente inserisce)

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -1,13 +1,36 @@
 
 
 let numeroRandom = []
+let numeriUtente = []
 
-for(i=0; i<=5; i++ ){
-
+do{
+     let numero = 0
      numero = Math.floor(Math.random()*100)
      alert(numero)
      numeroRandom.push(numero)
-}
+
+}while(numeroRandom.length<5)
+     
+
 
 console.log(numeroRandom)
 
+let clock ;
+
+clock = setInterval(function(){
+
+
+
+},3000)
+
+
+
+do{
+     let x = 0
+     x = prompt("inserisci numero")
+     numeriUtente.push(x)
+
+}while(numeriUtente.length<5)
+
+
+console.log(numeriUtente)

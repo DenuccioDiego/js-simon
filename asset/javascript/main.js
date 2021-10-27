c6a251812f7b97ab0eddfc0bc6b06ebe0d48605a
DenuccioDiego <diegodenuccio95@gmail.com>
2021-10-27 16:06:31 +0200

    esercizio completo

--- asset/javascript/main.js
+++ asset/javascript/main.js
@@ -4,6 +4,7 @@ let numeroRandom = []
 let numeriUtente = []
 let clock ;
 let seconds = -1
+let selezioneTimer = document.getElementById("timer")
 
 function chiediUtenteNumero(){
 
@@ -35,28 +36,29 @@ console.log(numeroRandom)
 clock = setInterval(function(){
 
      if(seconds<30){
-
           seconds++
-          document.getElementById("timer").innerHTML = seconds
-          //console.log(document.getElementById("timer").innerHTML = seconds)
-
-          
-          
-
+          selezioneTimer.innerHTML = seconds
      }
 
      else if(seconds==30){
-          document.getElementById("timer").innerHTML = "Ora tocca a te"
+          selezioneTimer.innerHTML = "Ora tocca a te"
           seconds++
      }
 
      else{
           clearInterval(clock)
-          
+     
           chiediUtenteNumero()
+
           let intersection = numeroRandom.filter(x => numeriUtente.includes(x));
           console.log(intersection)
-          
+
+          selezioneTimer.innerHTML = `Numero di valori indovinati ${intersection.length}<br>I numeri indovinati:`
+
+          intersection.forEach(x => {
+               selezioneTimer.insertAdjacentHTML("beforeend", `<div>${x}</div> `)
+          });
+    
      } 
 
 },1000)
